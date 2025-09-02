db8075a737c471781bd26b4d1bb3255657586a8d
Hollandcm1 <chris_h1996@hotmail.com>
2025-09-02 15:42:24 -0300

    updates to example scripts and figures

--- examples/example_pipeline.r
+++ examples/example_pipeline.r
@@ -29,7 +29,10 @@ result <- run_ks(df,
 save_path <- here("examples", "example_output")
 dir.create(save_path, recursive = TRUE, showWarnings = FALSE)
 # save the "combined_plot_points" plot for each group (only one group in this case)
-ggsave(file.path(save_path, "combined_plot_points.png"), plot = result$overall$visualizations$combined_plot_points)
+ggsave(file.path(save_path, "cosine_plot.png"), plot = result$overall$visualizations$cosine_plot)
+ggsave(file.path(save_path, "euclid_plot.png"), plot = result$overall$visualizations$euclid_plot)
+ggsave(file.path(save_path, "combined_plot.png"), plot = result$overall$visualizations$combined_plot)
+ggsave(file.path(save_path, "cosine_colored_by_euclid_plot.png"), plot = result$overall$visualizations$cosine_colored_by_euclid_plot)
 
 
 # Access to the underlying data is also available
